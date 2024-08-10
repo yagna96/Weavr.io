35655633a5f286c69ffe38c6b8066b646854a52a
yagna96 <prabhalayagna@gmail.com>
2024-08-10 04:09:55 +0100

    changes

--- sourcing.py
+++ sourcing.py
@@ -1,15 +1,31 @@
+
+"""
+- Script is used for data enrichment.
+- Created crad activity infomation including last_use as column to monitor card activity for both active and in active users
+- Created transactions duration data  to monitor settlement cycle time 
+
+"""
+
+
 import pandas as pd 
 import random as rand 
 from datetime import timedelta,datetime
 
 
-def create_transaction_time_table(data_path):
+def create_transaction_time_table(data_path :str) -> pd.DataFrame:
+     """
+     Function that generates dataframe which has transaction_id , creation_timesatmp and end_timestamp
+     """
      transaction =  pd.read_excel(data_path, sheet_name ='TRANSACTION')
      transact_time = transaction[['TRANSACTION_ID','CREATION_TIMESTAMP']]
      transact_time['END_TIMESTAMP'] = transact_time['CREATION_TIMESTAMP'].apply(lambda x: x+rand.randint(0,6000))
      return transact_time
 
-def create_time_offset(row):
+def create_time_offset(row : pd.Series):
+     """
+     Function returns random date from past 3 months for active users 
+     returns random date from past 6 months for inactive users  
+     """
      if row == 1:
           start_date = datetime.now()
      else:
@@ -17,7 +33,10 @@ def create_time_offset(row):
      time_stamp = start_date - timedelta(days = rand.randint(0,90))
      return time_stamp
           
-def create_card_activity(data_path):
+def create_card_activity(data_path :str)-> pd.DataFrame:
+     """
+     Function creates dataframe which has card_account_id,user_id,friendly_name,is_active,last_use.
+     """
      card_account = pd.read_excel(data_path, sheet_name ='CARD_ACCOUNT')
      card_activity = card_account[['CARD_ACCOUNT_ID','USER_ID','FRIENDLY_NAME','IS_ACTIVE']]
      card_activity['LAST_USE'] = card_activity['IS_ACTIVE'].apply(create_time_offset )
--- transformation.py
+++ transformation.py
@@ -1,4 +1,6 @@
-
+"""
+Script that transform the sample data file into various csv file . Csv files are stored in Result folder 
+"""
 import pandas as pd 
 from datetime import datetime
 from sourcing import create_card_activity,create_transaction_time_table
@@ -6,7 +8,10 @@ from sourcing import create_card_activity,create_transaction_time_table
 data_path = 'data/sample-files.xlsx'
 
 
-def transform_user_profile(data_path):
+def transform_user_profile(data_path :str):
+    """
+    Function to transform user profile 
+    """
     user_profile = pd.read_excel(data_path, sheet_name ='USER')
     # find null values in user_id to drop them  
     # considering it primary key for sql table
@@ -14,7 +19,10 @@ def transform_user_profile(data_path):
     #combining first and last names 
     user_profile.to_csv('Result/user_profile.csv')
 
-def transform_bank_account(data_path):
+def transform_bank_account(data_path :str):
+    """
+    Function to transform bank account 
+    """
     bank_account = pd.read_excel(data_path, sheet_name ='BANK_ACCOUNT')
     # bank account and user id should be non null 
     bank_account = bank_account.dropna(subset=['BANK_ACCOUNT_ID','USER_ID'])
@@ -22,7 +30,10 @@ def transform_bank_account(data_path):
     bank_account =bank_account.drop_duplicates(subset=['BANK_ACCOUNT_ID','USER_ID'])
     bank_account.to_csv('Result/bank_account.csv')
 
-def transform_card_account(data_path):
+def transform_card_account(data_path :str):
+    """
+    Function to transform card account 
+    """
     card_account = pd.read_excel(data_path, sheet_name ='CARD_ACCOUNT')
     # card account and user id should be non null ss
     card_account = card_account.dropna(subset=['CARD_ACCOUNT_ID','USER_ID'])
@@ -32,28 +43,38 @@ def transform_card_account(data_path):
     card_account =card_account[card_account['CARD_TYPE'].isin(accepted_card_types)]
     card_account.to_csv('Result/card_account.csv')
 
-def transform_transaction(data_path):
+def transform_transaction(data_path :str):
+    """
+    Function to transform transaction data
+    """
     transaction = pd.read_excel(data_path, sheet_name ='TRANSACTION')
     transaction.rename(columns={'CREATION TIMESTAMP':'CREATION_TIMESTAMP'})
     # card account and account id should be non null 
     transaction = transaction.dropna(subset=['TRANSACTION_ID','ACCOUNT_ID'])
     # (card accountid, account id) need to be unique 
     transaction=transaction.drop_duplicates(subset=['TRANSACTION_ID','ACCOUNT_ID'])
+    # checking acceptable values 
     accepted_transaction_types= ['SETTLEMENT','DEPOSIT']
     transaction =transaction[transaction['TRANSACTION_TYPE'].isin(accepted_transaction_types)]
     transaction.to_csv('Result/transactions.csv')
 
-def transform_transaction_time(data_path):
+def save_transaction_time(data_path :str):
+    """
+    Function to save transaction duration data
+    """
     transaction_time =create_transaction_time_table(data_path)
     transaction_time.to_csv('Result/transactions_time.csv')
 
-def tranform_card_activity(data_path):
+def save_card_activity(data_path :str):
+    """
+    Function to save card activity data
+    """
     card_activity = create_card_activity(data_path)
     card_activity.to_csv('Result/card_activity.csv')
 
 
-tranform_card_activity(data_path=data_path)
-transform_transaction_time(data_path=data_path)
+save_card_activity(data_path=data_path)
+save_transaction_time(data_path=data_path)
 transform_transaction(data_path=data_path)
 transform_card_account(data_path=data_path)
 transform_bank_account(data_path=data_path)
